63fee01e5254ad219d0c9f93c7c12362b11be388
Sam Flomenberg <sam.flomenberg@gmail.com>
2020-08-18 21:11:14 -0400

    feat: add Worker struct to send code to Thread

--- chapter-20/hello/src/lib.rs
+++ chapter-20/hello/src/lib.rs
@@ -1,7 +1,7 @@
 use std::{error::Error, fmt, thread};
 
 pub struct ThreadPool {
-    threads: Vec<thread::JoinHandle<()>>,
+    workers: Vec<Worker>,
 }
 
 impl ThreadPool {
@@ -19,13 +19,14 @@ impl ThreadPool {
             });
         }
 
-        let mut threads = Vec::with_capacity(size);
+        let mut workers = Vec::with_capacity(size);
 
-        for _ in 0..size {
+        for id in 0..size {
             // create some threads and store them in the vector
+            workers.push(Worker::new(id));
         }
 
-        Ok(ThreadPool { threads })
+        Ok(ThreadPool { workers })
     }
 
     pub fn execute<F>(&self, f: F)
@@ -44,6 +45,19 @@ impl Error for PoolCreationError {}
 
 impl fmt::Display for PoolCreationError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Oh no, something bad went down")
+        write!(f, "An error occurred when creating a ThreadPool")
+    }
+}
+
+struct Worker {
+    id: usize,
+    thread: thread::JoinHandle<()>,
+}
+
+impl Worker {
+    fn new(id: usize) -> Worker {
+        let thread = thread::spawn(|| {});
+
+        Worker { id, thread }
     }
 }
